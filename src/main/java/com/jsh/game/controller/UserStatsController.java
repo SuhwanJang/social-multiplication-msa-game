6902e9faf5b2fc13af8018c37dd99f163695ddaa
suhwanJang <suhwan9185@gmail.com>
2022-07-09 16:38:26 +0900

    contoller layer 추가

---
+++ src/main/java/com/jsh/game/controller/UserStatsController.java
@@ -0,0 +1,24 @@
+package com.jsh.game.controller;
+
+import com.jsh.game.domain.GameStats;
+import com.jsh.game.service.GameService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/stats")
+public class UserStatsController {
+
+    private final GameService gameService;
+
+    public UserStatsController(final GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @GetMapping
+    public GameStats getStatsForUser(@RequestParam("userId") final Long userId) {
+        return gameService.retrieveStatsForUser(userId);
+    }
+}
