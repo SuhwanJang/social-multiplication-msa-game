9fa45b6b229c4ab480c49dfc9339371590cc9c98
suhwanJang <suhwan9185@gmail.com>
2022-07-09 17:46:30 +0900

    EventHandler 추가

---
+++ src/main/java/com/jsh/game/event/EventHandler.java
@@ -0,0 +1,32 @@
+package com.jsh.game.event;
+
+import com.jsh.game.service.GameService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class EventHandler {
+
+    private GameService gameService;
+
+    EventHandler(final GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @RabbitListener(queues = "${multiplication.queue}")
+    void handleMultiplicationSolved(final MultiplicationSolvedEvent event) {
+        log.info("Multiplication Solved Event 수신: {}",
+                event.getMultiplicationResultAttemptId());
+        try {
+            gameService.newAttemptForUser(event.getUserId(),
+                    event.getMultiplicationResultAttemptId(),
+                    event.isCorrect());
+        } catch (final Exception e) {
+            log.error("MultiplicationSolvedEvent 처리 시 에러", e);
+            throw new AmqpRejectAndDontRequeueException(e);
+        }
+    }
+}
